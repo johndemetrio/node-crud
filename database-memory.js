7981e5b15e1a3dbf4cbb8a15e288f3b1c788f0af
John Demetrio <johndemetrio123@gmail.com>
2023-10-20 21:36:55 -0300

    metodos/rotas POST GET PUT DELETE

--- database-memory.js
+++ database-memory.js
@@ -4,7 +4,15 @@ export class DatabaseMemory{
     #notes = new Map()
 
     list(){
-        return this.#notes.values
+        return Array.from(this.#notes.entries()).map((noteArray) => {
+            const id = noteArray[0]
+            const data = noteArray[1]
+
+            return{
+                id,
+                ...data
+            }
+        })
     }
     create(note){
         const noteId = randomUUID()
--- server.js
+++ server.js
@@ -1,27 +1,41 @@
-//import { createServer } from 'node:http'
-
-//const server = createServer((request, response) => {
-//    response.write('oi')
-
-//    return response.end()
-//})
-
-//server.listen(3333)
 import { fastify } from "fastify";
 import { DatabaseMemory } from "./database-memory.js";
 
 const server = fastify()
 const database = new DatabaseMemory()
 
-server.post('/notes', () =>{
+server.post('/notes', (request, reply) => {
+    const { title, content } = request.body
     database.create({
-        title:'nota 1',
-        content: 'essa nota é um teste para o crud em node.js',
+        title,
+        content,
     })
-    console.log(database.list())
+    return reply.status(201).send()
 })
-server.get('/', () =>{
-    return 'hello world'
+
+server.get('/notes', (request, reply) =>{
+    const notes = database.list()
+
+    console.log(notes)
+
+    return notes
+})
+
+server.put('/notes/:id', (request, reply) =>{
+    const noteId = request.params.id
+    const { title, content } = request.body
+    database.update(noteId, {
+        title,
+        content,
+    })
+    return reply.status(204).send()
+})
+server.delete('/notes/:id', (request, reply) =>{
+    const noteId = request.params.id
+
+    database.delete(noteId)
+
+    return reply.status(204).send()
 })
 
 server.listen({
